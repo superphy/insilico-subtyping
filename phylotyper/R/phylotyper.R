14e815ffb26553f7b056ce15d8ba2b84068cc86a
mwhitesi <mdwhitsi@gmail.com>
2016-01-26 17:38:47 -0700

    Started a test workflow - need to finish the summary function

--- phylotyper/R/phylotyper.R
+++ phylotyper/R/phylotyper.R
@@ -22,27 +22,90 @@
 # Create enviroment to keep phylotyper functions
 phylotyper = new.env()
 
-phylotyper$makePriors = function(tree, x) {
+
+phylotyper$runSubtypeProcedure = function(tree, priorM, scheme=1) {
+	# A Wrapper that runs one of several possible subtype 
+	# estimation procedures
 	#
+	# Args:
+	#  tree: phylo object containing subtype tree
+	#  priorM: a matrix of prior values indicating tip node subtype state
+	#  scheme: an integer between 1:? indicating procedure to use.
+	#    Possible procedures (phytool method, model):
+	#      1=rerootingMethod, ER
+	#      2=rerootingMethod, SYM
 	#
-	#
-	#
+	# Returns list with:
+	#   ?
 	#
 
+	if(scheme == 1) {
+		# Rerooting method, ER model
+		fit = rerootingMethod(tree, priorM, model='ER', tips=TRUE)
+		return(list(result=fit, tip.pp=fit$marginal.anc[tree$tip.label,], plot.function='plotRR'))
+
+	} else if(scheme == 2) {
+		# Rerooting method, SYM model
+		fit = rerootingMethod(tree, priorM, model='SYM', tips=TRUE)
+		return(list(result=fit, tip.pp=fit$marginal.anc[tree$tip.label,], plot.function='plotRR'))
+	}
+
+}
+
+phylotyper$makePriors = function(tree, subtypes) {
+	# Generate prior matrix used as input for ACE
+	# 
+	# Tree tip nodes with no subtype in x, will be assigned
+	# flat priors (equal likelihood of having any subtype)
+	#
+	# Args:
+	#  tree: phylo object containing subtype tree
+	#  subtypes: factor list of subtype assignments. List names
+	#    must match tip names in tree, except for tips
+	#    with unknown subtypes (which subtypes
+	#    will be predicted for)
+	#
+	# Returns list with:
+	#   prior.matrix: matrix containing prior values
+	#   untyped: vector of tree tip names with no subtype 
+	#
 	
 	# Find leaves with no subtype
-	undefined = setdiff(tree$tip.label rownames(x))
+	undefined = setdiff(tree$tip.label, names(x))
 
 	# Build prior matrix
 	priors <- to.matrix(x,seq=levels(x))
 
 	# Add flat priors for leaves with no subtype
-	priors <- rbind(prior, matrix(1/length(levels(x)),
-		length(undefined), 2, dimnames=list(undefined)))
+	numstates = length(levels(x))
+	if(length(undefined) > 0)
+		priors <- rbind(priors, matrix(1/numstates,
+			length(undefined), numstates, dimnames=list(undefined)))
 
 
-	return(prior.matrix=priors, untyped=undefined)
-} 
+	return(list(prior.matrix=priors, untyped=undefined))
+}
+
+phylotyper$plotRR = function(tree, result, plot.nodes=TRUE) {
+	# plot tree with posterior probabilities from 
+	# rerootingMethod function displayed as pies
+	#
+	# Args:
+	#  tree: phylo object containing subtype tree
+	#  result: output object from rerootingMethod()
+	#  plot.nodes: display interior tree node ancestral state
+	#    posterior probability
+	#
+	# Returns:
+	#   nothing
+	#
+
+	plot(tree)
+	tiplabels(pie=result$marginal.anc[tree$tip.label,], cex=0.5)
+	if(plot.nodes) {
+		nodelabels(pie=result$marginal.anc[as.character(1:tree$Nnode+Ntip(tree)),],cex=0.6)
+	}
+}
 
 phylotyper$loadInstallLibraries = function(libloc="~/R/", repo="http://cran.stat.sfu.ca/") {
 	# Attempt to load required libraries
@@ -59,7 +122,7 @@ phylotyper$loadInstallLibraries = function(libloc="~/R/", repo="http://cran.stat
 	# 
 
 	# Location to install libraries to
-	if((! is.null(libloc)) && libloc) {
+	if(! is.null(libloc)) {
 		.libPaths(c(libloc,.libPaths()))
 	}
 	
@@ -98,22 +161,156 @@ phylotyper$loadInstallLibraries = function(libloc="~/R/", repo="http://cran.stat
 		}
 	}
 	
-	NULL
+	TRUE
 }
 
-phylotyper$test_tree = function() {
+phylotyper$testTree = function() {
 
-	## simulate stochastic pure-birth tree
+	# Simulate stochastic pure-birth tree
 	tree<-pbtree(n=26,tip.label=LETTERS,scale=1)
-	## generate character transition matrix
-	Q<-matrix(c(-1,1,1,-1),2,2)
-	rownames(Q)<-colnames(Q)<-letters[1:2]
-	Q
+
+	# Generate character transition matrix
+	Q<-matrix(c(-1,0.8,0.2,0.8,-1.2,0.4,0.2,0.4,-0.6),3,3, dimnames=list(c("red","green","blue"),c("red","green","blue")))
+	
+	# Use transition matrix to simulate states on tree
+	y<-as.factor(sim.history(tree,Q)$states)
+
+	# Set some states as unknown
+	y.sampled<-sample(y,20)
+	y.sampled
 
 
+	return(list(tree=tree, subtypes=y.sampled))
 }
 
+phylotyper$testSimulation = function(tree, subtypes, scheme=1) {
+	# Estimate tip subtype posterior probability for each 
+	# by setting tip prior to flat/unassigned and running
+	# esimtation procedure
+	# 
+	#
+	# Args:
+	#  tree: phylo object containing subtype tree
+	#  subtypes: factor list of subtype assignments. List names
+	#    must match tip names in tree
+	#
+	# Returns list with:
+	#   prior.matrix: matrix containing prior values
+	#   untyped: vector of tree tip names with no subtype 
+	#
 
+	n = length(subtypes)
+
+	# Make prior matrix
+	priorR = phylotyper$makePriors(tree, subtypes)
+	priorM = priorR$prior.matrix
+	nstates = ncol(priorM)
+
+	# Make result matrix
+	pp = matrix(0,n,nstates)
+	rownames(pp) = names(subtypes)
+	colnames(pp) = colnames(priorM)
+
+	# Flat prior
+	flat = matrix(1/nstates,1,nstates)
+
+	for(i in 1:n) {
+
+		tip = names(subtypes)[i]
+		testprior = priorM
+		testprior[i,] = flat
+
+		testfit = phylotyper$runSubtypeProcedure(tree, testprior, scheme)
+
+		pp[i, ] = testfit$tip.pp[tip, ]
+
+		cat("Completed test iteration",i,"...\n")
+	}
+
+	return(pp)
+}
+
+phylotyper$simulationSummary = function(subtypes, pp) {
+	# Summarize performance of simulation
+	# 
+	# 
+	#
+	# Args:
+	#  subtypes: factor list of subtype assignments. List names
+	#    must match tip names in tree
+	#  pp: posterior probability for each subtype obtained for 
+	#    tip when true subtype masked and prediction procedure was run
+	#
+	# Returns list with:
+	#   prior.matrix: matrix containing prior values
+	#   untyped: vector of tree tip names with no subtype 
+	#
+
+	# Record frequency of correctly predicted tip subtype
+	# when selecting subtype with:
+	# 'max': highest posterior probability
+	# 'over50': highest posterior probability, when pp is > 50
+	# 'over70': highest posterior probability, when pp is > 70
+	# 'over90': highest posterior probability, when pp is > 90
+
+	
+	numCorrect = matrix(0, 3, 4)
+	rownames(numCorrect) = c('frequency', 'n', 'correct')
+	colnames(numCorrect) = c('max', 'over50', 'over70', 'over90')
+
+	subtype.states = colnames(pp)
+
+	for(i in names(subtypes)) {
+		st = subtypes[i]
+		mxi = which.max(pp[i,])
+		mx = pp[i,mxi]
+		
+		# Total
+		numCorrect['n', 'max'] = numCorrect['n', 'max'] + 1
+
+		# Correct
+		gotit = FALSE
+		if(st == subtype.states[mxi]) {
+			numCorrect['correct', 'max'] = numCorrect['correct', 'max'] + 1
+			gotit = TRUE
+		}
+
+		if(mx > 0.5) {
+			numCorrect['n', 'over50'] = numCorrect['n', 'over50'] + 1
+			if(gotit)
+				numCorrect['correct', 'over50'] = numCorrect['correct', 'over50'] + 1
+
+			if(mx > 0.7) {
+				numCorrect['n', 'over70'] = numCorrect['n', 'over70'] + 1
+				if(gotit)
+					numCorrect['correct', 'over70'] = numCorrect['correct', 'over70'] + 1
+
+				if(mx > 0.9) {
+					numCorrect['n', 'over90'] = numCorrect['n', 'over90'] + 1
+					if(gotit)
+						numCorrect['correct', 'over90'] = numCorrect['correct', 'over90'] + 1
+
+				}
+			}
+		}
+
+		# Frequencies
+		numCorrect['frequency',] = apply(numCorrect, 2, function(x) if(x['n'] == 0){ 0 } else { x['correct']/x['n'] } )
+
+	}
+
+	# Check accuracy of posterior probabilities
+	w.size = 2 
+	for(j in seq(from=w.size, to=(100-w.size), by=2) ) {
+		l = j-w.size
+		u = j+w.size
+
+		apply(
+	}
+
+
+	return(numCorrect)
+}
 
 
 while("phylotyper" %in% search())
